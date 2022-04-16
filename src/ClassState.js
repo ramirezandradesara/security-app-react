524d13160832f967f7451bff51e9008184423b89
ramirezandradesara <sararamirezandrade@gmail.com>
2022-04-16 16:22:33 -0300

    estados compuestos e independientes

--- src/ClassState.js
+++ src/ClassState.js
@@ -1,22 +1,25 @@
 import React from "react";
 import { Loading } from "./Loading";
 
+const SECURITY_CODE = '123'
+
 export default class ClassState extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      error: true,
+    this.state = { // estado compuesto: todos los estados estan contenidos en un objeto
+      value: "",
+      error: false,
       loading: false,
     };
   }
 
-  UNSAFE_componentWillMount() {
-    console.log("UNSAFE_componentWillMount");
-  }
+  // UNSAFE_componentWillMount() {
+  //   console.log("UNSAFE_componentWillMount");
+  // }
 
-  componentDidMount() {
-    console.log("componentDidMount");
-  }
+  // componentDidMount() {
+  //   console.log("componentDidMount");
+  // }
 
   componentDidUpdate() {
     console.log("Update");
@@ -25,21 +28,29 @@ export default class ClassState extends React.Component {
       setTimeout(() => {
         console.log("Doing the validation");
 
-        this.setState( { loading: false } );
+     if(SECURITY_CODE === this.state.value) {
+        this.setState({ loading: false, error: false });
+     }else{
+        this.setState({ loading: false, error: true });
+     }
+
+    // this.setState({ loading: false, error: SECURITY_CODE !== this.state.value }); // Estados complejos o compuestos: es la modficacion de 2 o más estados utilizando la siguiente sintaxis:
 
         console.log("Finishing the validation");
-      }, 3000);
+      }, 1000);
     }
   }
 
   render() {
+    // const { error, loading, value } = this.state; otra forma de trabajar para no escribir this.state.estado
+
     return (
       <div>
         <h2>Delete {this.props.name}</h2>
 
         <p>Please enter the security code</p>
 
-        {this.state.error && (
+        {(this.state.error && !this.state.loading) && (
           <p>Error: Security code is incorrect</p>
         )}
 
@@ -47,7 +58,12 @@ export default class ClassState extends React.Component {
           <Loading />
         )}
 
-        <input placeholder="Security Code" />
+        <input placeholder="Security Code"
+        value={this.state.value}
+        onChange={(e) => {
+          this.setState({ value: e.target.value });
+        }}
+        />
 
         <button
           onClick={() => this.setState({ loading: true })}
--- src/UseState.js
+++ src/UseState.js
@@ -3,29 +3,43 @@ import React, { useEffect } from 'react'
 const SECURITY_CODE = '123'
 
 export default function UseState({ name }) {
-  // llamamos independientes a los estados porque tenemos un actualizador por cada uno, a diferencia de class components
+  // estaods independientes o siples: tenemos un actualizador por cada uno, a diferencia de class components
   const [value, setValue] = React.useState('')
   const [error, setError] = React.useState(false)
   const [loading, setLoading] = React.useState(false)
 
-  console.log(value);
+  // estado compuesto o complejos
+  const [state, setState] = React.useState({
+    value: '',
+    error: false,
+    loading: false,
+    deleted: false,
+    confirmed: false,
+  })
+
+  console.log(state);
 
   React.useEffect(() => {
     console.log("Starting the effect");
 
-    if (!!loading) {
+    if (!!state.loading) {
       setTimeout(() => {
         console.log("Doing the validation");
 
-        //value === SECURITY_CODE ? setError(false) : setError(true)
-   
-        if(value === SECURITY_CODE) {
-          setLoading(false);
-          setError(false);
-          
-        }else{
-          setError(true);
-          setLoading(false);
+        if (state.value === SECURITY_CODE) {
+          setState({
+            ...state,
+            loading: false,
+            error: false,
+            confirmed: true
+          }); // en estados compuestos no basta modificar una de sus entradas, debemos crear una nueva coleccion y hacer que nuestro estado apunte a ella 
+          // setLoading(false);
+          // setError(false);
+
+        } else {
+          setState({ ...state, loading: false, error: true });
+          // setError(true);
+          // setLoading(false);
         }
 
 
@@ -34,34 +48,62 @@ export default function UseState({ name }) {
     }
 
     console.log("Finishing the effect");
-  }, [loading]);
+  }, [state.loading]);
 
 
+  if (!state.deleted && !state.confirmed) {
+    return (
+      <div>
+        <h2>Eliminar {name}</h2>
+        <p>Por favor, escriba el código de seguridad.</p>
 
-  return (
-    <div>
-      <h2>Eliminar {name}</h2>
-      <p>Por favor, escriba el código de seguridad.</p>
+        {(state.error && !state.loading) && (
+          <p>Código incorrecto</p>
+        )}
 
-      {(error && !loading) && (
-        <p>Código incorrecto</p>
-      )}
+        {state.loading && (<p>Cargando...</p>)}
 
-      {loading && (<p>Cargando...</p>)}
-      <input
-        type='text'
-        placeholder='código de seguridad' 
-        value={value}
-        onChange={(e) => {
-          // setError(false) siempre estamos llamando al estado, lo que renderiza innecesariamente
-          setValue(e.target.value)
-        }}
+        <input
+          type='text'
+          placeholder='código de seguridad'
+          value={state.value}
+          onChange={(e) => {
+            // setError(false) siempre estamos llamando al estado, lo que renderiza innecesariamente
+            // setValue(e.target.value)
+            setState({ ...state, value: e.target.value })
+          }}
         />
-      <button
-        onClick={() => 
-          setLoading(true)
-         // setError(false);} siempre muestra el cartelito, desaparece cuadno es correcto el codigo
-        }>Comprobar</button>
-    </div>
-  )
+
+        <button
+          onClick={() =>
+            setState({ ...state, loading: true })
+            // setLoading(true)
+            // setError(false);} siempre muestra el cartelito, desaparece cuadno es correcto el codigo
+          }>Comprobar</button>
+      </div>
+    )
+  } else if (state.confirmed && !state.deleted) {
+    return (
+      <React.Fragment>
+        <p>¿Estas segurx de elimnar?</p>
+        <button
+          onClick={() => setState({ ...state, deleted: true,  })}
+        >Sí, eliminar</button>
+
+        <button
+          onClick={() => setState({ ...state, confirmed: false, value: '' })}
+        >Nop, me arrenpentí</button>
+      </React.Fragment>
+
+    )
+  } else {
+    return (
+      <React.Fragment>
+        <p>Eliminado con éxito</p>
+        <button
+          onClick={() => setState({ ...state, confirmed: false, deleted: false, value: ''})}
+        >Recuperar useState</button>
+      </React.Fragment>
+    )
+  }
 }
