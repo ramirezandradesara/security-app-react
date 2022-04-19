05c9621d8e6b589c0a3d43275eb83b4a1d110b1f
ramirezandradesara <sararamirezandrade@gmail.com>
2022-04-18 23:14:33 -0300

    estados semideclarativos con useState

--- src/UseState.js
+++ src/UseState.js
@@ -19,6 +19,53 @@ export default function UseState({ name }) {
 
   console.log(state);
 
+  // código declarativo
+  const onConfirm = () => {
+    setState({
+      ...state,
+      loading: false,
+      error: false,
+      confirmed: true
+    });
+  }
+
+  const onError = () => {
+    setState({ 
+      ...state, 
+      loading: false, 
+      error: true });
+  }
+
+  const onWrite = (newValue) => {
+    setState({ ...state, value: newValue })
+  }
+
+  const onDelete = () => {
+    setState({ 
+      ...state, 
+      deleted: true,  })
+  }
+
+  const onCheck = () => {
+    setState({ ...state, loading: true })
+    // setTimeout(() => {
+    //   if (value === SECURITY_CODE) {
+    //     onConfirm();
+    //   } else {
+    //     onError();
+    //   }
+    // }, 2000);
+  }
+
+  const onReset = () => {
+    setState({ 
+      ...state, 
+      confirmed: false,
+      deleted: false, 
+      value: '',
+    })
+  }
+
   React.useEffect(() => {
     console.log("Starting the effect");
 
@@ -27,17 +74,21 @@ export default function UseState({ name }) {
         console.log("Doing the validation");
 
         if (state.value === SECURITY_CODE) {
-          setState({
-            ...state,
-            loading: false,
-            error: false,
-            confirmed: true
-          }); // en estados compuestos no basta modificar una de sus entradas, debemos crear una nueva coleccion y hacer que nuestro estado apunte a ella 
+            onConfirm();
+
+          // setState({   // en lugar de pasar el objeto completo, pasamos la función onConfirm
+          //   ...state,
+          //   loading: false,
+          //   error: false,
+          //   confirmed: true
+          // }); 
+          // en estados compuestos no basta modificar una de sus entradas, debemos crear una nueva coleccion y hacer que nuestro estado apunte a ella 
           // setLoading(false);
           // setError(false);
 
         } else {
-          setState({ ...state, loading: false, error: true });
+          onError();
+          // setState({ ...state, loading: false, error: true });
           // setError(true);
           // setLoading(false);
         }
@@ -70,13 +121,15 @@ export default function UseState({ name }) {
           onChange={(e) => {
             // setError(false) siempre estamos llamando al estado, lo que renderiza innecesariamente
             // setValue(e.target.value)
-            setState({ ...state, value: e.target.value })
+            // setState({ ...state, value: e.target.value })
+            onWrite(e.target.value)
           }}
         />
 
         <button
           onClick={() =>
-            setState({ ...state, loading: true })
+            onCheck()
+            //setState({ ...state, loading: true })
             // setLoading(true)
             // setError(false);} siempre muestra el cartelito, desaparece cuadno es correcto el codigo
           }>Comprobar</button>
@@ -87,11 +140,12 @@ export default function UseState({ name }) {
       <React.Fragment>
         <p>¿Estas segurx de elimnar?</p>
         <button
-          onClick={() => setState({ ...state, deleted: true,  })}
-        >Sí, eliminar</button>
-
+         onClick={() => onDelete()}> Sí, eliminar</button>
+        {/* onClick={() => setState({ ...state, deleted: true,  })} */}
+         
+       
         <button
-          onClick={() => setState({ ...state, confirmed: false, value: '' })}
+          onClick={() => onReset()}
         >Nop, me arrenpentí</button>
       </React.Fragment>
 
@@ -101,7 +155,7 @@ export default function UseState({ name }) {
       <React.Fragment>
         <p>Eliminado con éxito</p>
         <button
-          onClick={() => setState({ ...state, confirmed: false, deleted: false, value: ''})}
+          onClick={() => onReset()}
         >Recuperar useState</button>
       </React.Fragment>
     )
